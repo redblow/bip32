71c2630dcdc60b0191d4a49356899ea1c95eefd5
redblow <redblow@163.com>
2021-01-22 11:39:43 +0800

    替换为crypto-js

--- src/crypto.js
+++ src/crypto.js
@@ -1,26 +1,33 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const createHash = require('create-hash');
-const createHmac = require('create-hmac');
+const CryptoJS = require('crypto-js');
 function hash160(buffer) {
-    const sha256Hash = createHash('sha256')
-        .update(buffer)
-        .digest();
-    try {
-        return createHash('rmd160')
-            .update(sha256Hash)
-            .digest();
-    }
-    catch (err) {
-        return createHash('ripemd160')
-            .update(sha256Hash)
-            .digest();
-    }
+    const sha256Hash = CryptoJS.SHA256(buffer);
+    return CryptoJS.RIPEMD160(sha256Hash);
 }
 exports.hash160 = hash160;
 function hmacSHA512(key, data) {
-    return createHmac('sha512', key)
-        .update(data)
-        .digest();
+    return CryptoJS.HmacSHA512(data, key);
 }
 exports.hmacSHA512 = hmacSHA512;
+// const createHash = require('create-hash');
+// const createHmac = require('create-hmac');
+// export function hash160(buffer: Buffer): Buffer {
+//   const sha256Hash: Buffer = createHash('sha256')
+//     .update(buffer)
+//     .digest();
+//   try {
+//     return createHash('rmd160')
+//       .update(sha256Hash)
+//       .digest();
+//   } catch (err) {
+//     return createHash('ripemd160')
+//       .update(sha256Hash)
+//       .digest();
+//   }
+// }
+// export function hmacSHA512(key: Buffer, data: Buffer): Buffer {
+//   return createHmac('sha512', key)
+//     .update(data)
+//     .digest();
+// }
--- ts-src/crypto.ts
+++ ts-src/crypto.ts
@@ -1,23 +1,35 @@
-const createHash = require('create-hash');
-const createHmac = require('create-hmac');
+const CryptoJS = require('crypto-js');
 
 export function hash160(buffer: Buffer): Buffer {
-  const sha256Hash: Buffer = createHash('sha256')
-    .update(buffer)
-    .digest();
-  try {
-    return createHash('rmd160')
-      .update(sha256Hash)
-      .digest();
-  } catch (err) {
-    return createHash('ripemd160')
-      .update(sha256Hash)
-      .digest();
-  }
+  const sha256Hash: Buffer = CryptoJS.SHA256(buffer);
+  return CryptoJS.RIPEMD160(sha256Hash);
 }
 
 export function hmacSHA512(key: Buffer, data: Buffer): Buffer {
-  return createHmac('sha512', key)
-    .update(data)
-    .digest();
+  return CryptoJS.HmacSHA512(data,key);
 }
+
+
+// const createHash = require('create-hash');
+// const createHmac = require('create-hmac');
+
+// export function hash160(buffer: Buffer): Buffer {
+//   const sha256Hash: Buffer = createHash('sha256')
+//     .update(buffer)
+//     .digest();
+//   try {
+//     return createHash('rmd160')
+//       .update(sha256Hash)
+//       .digest();
+//   } catch (err) {
+//     return createHash('ripemd160')
+//       .update(sha256Hash)
+//       .digest();
+//   }
+// }
+
+// export function hmacSHA512(key: Buffer, data: Buffer): Buffer {
+//   return createHmac('sha512', key)
+//     .update(data)
+//     .digest();
+// }
